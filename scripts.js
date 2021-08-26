f2a39a696b98cb7d51ffc1a49e273daa94eed6a2
Sarah dePalo <sarahdepalo99@gmail.com>
2021-08-26 15:30:06 -0400

    initial commit and finished navbar

---
+++ scripts.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementsByClassName('toggleButton')[0];
+    const navbarLinks = document.getElementsByClassName('navbarLinks')[0];
+
+    toggleButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active');
+    })
+});
